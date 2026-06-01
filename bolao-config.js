b0b94e3c7e474ce335474eded6381464a81da92d
gutoassumpcao <antonioafassumpcao@gmail.com>
2026-06-01 10:59:05 -0400

    Create bolao-config.js

---
+++ bolao-config.js
@@ -0,0 +1,9 @@
+window.BOLAO_CONFIG = {
+  apiKey: "AIzaSyCKYua0XZXt81c5mAZBnw80lTtz2NnFQYU",
+  authDomain: "bolao-da-copa-guto.firebaseapp.com",
+  projectId: "bolao-da-copa-guto",
+  storageBucket: "bolao-da-copa-guto.firebasestorage.app",
+  messagingSenderId: "254625317991",
+  appId: "1:254625317991:web:06405cbe8916cb850500c2"
+};
+
